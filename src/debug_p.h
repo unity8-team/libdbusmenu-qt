06c56d2e1732275a86c93d3cf2df85ae3e7ac898
gateau <gateau@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2010-02-05 11:32:50 -0800

    Nicer debug tools
    git-svn-id: https://svn.kde.org/home/kde/branches/work/~agateau/dbusmenu/kdesupport@1076629 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- src/debug_p.h
+++ src/debug_p.h
@@ -23,8 +23,16 @@
 
 #include <QDebug>
 
+#define _DMBLUE  "\033[34m"
+#define _DMRED   "\033[31m"
+#define _DMRESET "\033[0m"
+#define _DMTRACE(level, color) (level().nospace() << color << __PRETTY_FUNCTION__ << _DMRESET ":").space()
+
 // Simple macros to get KDebug like support
-#define DMDEBUG qDebug() << __FUNCTION__
-#define DMWARNING qWarning() << __FUNCTION__
+#define DMDEBUG   _DMTRACE(qDebug, _DMBLUE)
+#define DMWARNING _DMTRACE(qWarning, _DMRED)
+
+// Log a variable name and value
+#define DMVAR(var) DMDEBUG << #var ":" << var
 
 #endif /* DEBUG_P_H */
