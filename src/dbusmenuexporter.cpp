a13e55aa0b9bcc4a8f56aa5b584be7e6accc603a
gateau <gateau@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2010-02-05 11:32:51 -0800

    Fix KDE title support
    git-svn-id: https://svn.kde.org/home/kde/branches/work/~agateau/dbusmenu/kdesupport@1084364 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- src/dbusmenuexporter.cpp
+++ src/dbusmenuexporter.cpp
@@ -100,6 +100,7 @@ public:
         } else if (type == "text") {
             names = QStringList()
                 << "label"
+                << "enabled"
                 << "icon-name"
                 << "icon-data"
                 ;
