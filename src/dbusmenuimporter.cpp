a96deaa594ddebfa560ac068a1d9b901639ef3a7
Aurelien Gateau <aurelien.gateau@canonical.com>
2011-02-23 18:44:11 +0100

    Avoid infinite loop

--- src/dbusmenuimporter.cpp
+++ src/dbusmenuimporter.cpp
@@ -437,10 +437,6 @@ void DBusMenuImporter::GetLayoutCallback(int parentId, QDBusPendingCallWatcher *
         connect(action, SIGNAL(triggered()),
             &d->m_mapper, SLOT(map()));
         d->m_mapper.setMapping(action, dbusMenuItem.id);
-
-        if (action->menu()) {
-            d->refresh(dbusMenuItem.id);
-        }
     }
     #ifdef BENCHMARK
     DMDEBUG << "- Menu filled:" << sChrono.elapsed() << "ms";
