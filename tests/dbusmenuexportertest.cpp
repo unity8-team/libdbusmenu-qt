9d7d55cae3789ccdf3ab798cf18242bc9ba25866
Aurelien Gateau <aurelien.gateau@canonical.com>
2011-08-30 12:00:12 +0200

    If menu layout is a-b-c, do not turn it into a-bc
    bzr-revid: aurelien.gateau@canonical.com-20110829140453-idva5je0sbkz8quf

--- tests/dbusmenuexportertest.cpp
+++ tests/dbusmenuexportertest.cpp
@@ -671,9 +671,12 @@ void DBusMenuExporterTest::testSeparatorCollapsing_data()
     QTest::addColumn<QString>("input");
     QTest::addColumn<QString>("expected");
 
+    QTest::newRow("one-separator") << "a-b" << "a-b";
+    QTest::newRow("two-separators") << "a-b-c" << "a-b-c";
     QTest::newRow("middle-separators") << "a--b" << "a-b";
     QTest::newRow("separators-at-begin") << "--a-b" << "a-b";
     QTest::newRow("separators-at-end") << "a-b--" << "a-b";
+    QTest::newRow("separators-everywhere") << "--a---bc--d--" << "a-bc-d";
     QTest::newRow("empty-menu") << "" << "";
 }
 
